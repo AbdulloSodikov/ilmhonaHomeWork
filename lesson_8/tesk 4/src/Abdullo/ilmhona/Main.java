06f143856e30e17e5827bdfc599b1809add275ee
AbdulloSodikov <sodikov.ak@gmail.com>
2020-10-20 10:45:14 +0500

    solved problem

---
+++ lesson_8/tesk 4/src/Abdullo/ilmhona/Main.java
@@ -0,0 +1,27 @@
+package Abdullo.ilmhona;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arrayInPosAndNegNumber = {1, -2, 3, -4, 5, -6, 7, -8, 9, -10, 20};  // Задача № 5
+        int[] arrayOnlyPosNumber = filterArray(arrayInPosAndNegNumber);
+
+    }
+    public  static int [] filterArray (int[] array) {
+        int counterPosNumber = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                counterPosNumber++;
+            }
+        }
+        int[] arrayPosNumber = new int[counterPosNumber];
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                arrayPosNumber[j] = array[i];
+                j++;
+            }
+        }
+        return arrayPosNumber;
+    }
+}

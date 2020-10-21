c0477269decfe7c6e5c442e76bb6271acb2c8572
AbdulloSodikov <sodikov.ak@gmail.com>
2020-10-21 14:49:24 +0500

    solved tesk 2

---
+++ lesson_10/tesk2/src/Abdullo/ilmhona/Main.java
@@ -0,0 +1,34 @@
+package Abdullo.ilmhona;
+
+public class Main {
+
+    public static void main(String[] args) {
+ Apple object1 = new Apple();
+    object1.variety = "Симеренко";
+    object1.color = "Зеленное";
+    object1.weight = 80;
+    object1.countryOfOrigin = "Таджикистан";
+
+ Apple object2 = new Apple();
+    object2.variety = "залатой";
+    object2.color  = "Желтое";
+    object2.weight = 50;
+    object2.countryOfOrigin = "Россия";
+
+ Apple object3 = new Apple();
+    object3.variety = "Палосатый";
+    object3.color = "красное";
+    object3.weight = 60;
+    object3.countryOfOrigin = "Китай";
+
+    
+    }
+}
+class Apple {
+
+    String  variety; // сорт
+    String color; // цвет
+    double weight; // вес
+    String countryOfOrigin; // страна происхождения
+
+}

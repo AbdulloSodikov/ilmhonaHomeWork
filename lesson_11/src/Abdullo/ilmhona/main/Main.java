c1fdadda0605c0e793e508828c575552ec999bea
AbdulloSodikov <sodikov.ak@gmail.com>
2020-11-04 15:47:39 +0500

    solved problem1

--- lesson_11/src/Abdullo/ilmhona/main/Main.java
+++ lesson_11/src/Abdullo/ilmhona/main/Main.java
@@ -6,8 +6,8 @@ class Main {
     public static void main(String[] args) {
 
         NewClass1 problem1 = new NewClass1();
-        problem1.x = 5;
-        problem1.y = 10;
+        problem1.x = 15;
+        problem1.y = 11;
         problem1.intXY();
         problem1.total();
         problem1.max();

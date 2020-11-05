cb7f735b3de68a5552e79b554982ebe5c99e0ce1
AbdulloSodikov <sodikov.ak@gmail.com>
2020-11-05 11:24:47 +0500

    update problem 2

--- lesson_11/problem 2/src/Abdullo/ilmhona/main/Main.java
+++ lesson_11/problem 2/src/Abdullo/ilmhona/main/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         Car object1 = new Car("Mercedes",2021);
-        System.out.println(object1.model);
+
 
         Car object2 = new Car();
-        System.out.println(object2.model);
+
     }
 }
--- lesson_11/problem 2/src/Abdullo/ilmhona/problem2/Car.java
+++ lesson_11/problem 2/src/Abdullo/ilmhona/problem2/Car.java
@@ -1,16 +1,18 @@
 package Abdullo.ilmhona.problem2;
 
 public class Car {
-    public String model;
-    public int year;
+    String model;
+    int year;
 
 public Car (String modelInCons,int yearInCon){
     model = modelInCons;
     year = yearInCon;
+    System.out.println(model + "\n" + year + "\n" );
 
 }
 public Car (){
     model = "BMW";
     year = 2020;
+    System.out.println(model + "\n" + year + "\n" );
 }
 }
